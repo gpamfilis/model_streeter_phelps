9a1cf37a2f786902c41112358f863626768d9078
George Pamfilis <gpamfilis@gmail.com>
2015-06-01 14:12:41 +0300

    fixed the velocity issue all results are good.

--- SP.py
+++ SP.py
@@ -8,11 +8,12 @@ from numpy.random import ranf
 import pandas as pd
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
-
-
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
 class StreeterPhelps(object):
+
     def __init__(self):
         self.data = pd.read_excel('Data/Streeter_Phelps_input.xlsx', sheetname='data')
         self.constants = pd.read_excel('Data/Streeter_Phelps_input.xlsx', sheetname='constants')
@@ -29,7 +30,8 @@ class StreeterPhelps(object):
         self.Lb = 4.0
         self.L = self.data['L-BOD'].values  # BOD
         self.log_L = np.log(self.L)
-        self.time = self.distance_meters/self.uav
+        self.time = self.distance_meters/(self.uav*60*60*24)
+        # self.uav
         self.DO = self.data['DO'].values
         # plt.scatter(distance_meters,log_L)
         self.kd = -linregress(self.distance_meters, self.log_L)[0]*self.uav*60*60*24
@@ -89,7 +91,7 @@ class StreeterPhelps(object):
 
         kamin1 = kas[np.where(RMSES == np.min(RMSES))[0]]  # minimum ka.
         print "the minimum ka is: ", kamin1, "and the corresponding root_mean_square_error is: ", np.min(RMSES)
-        cm = self.streeter_phelps(self.Csat, self.C0, self.L0, kamin1, self.kd, self.time)
+        cm = self.streeter_phelps(self.Csat, self.C0, self.L0, 0.2032, self.kd, self.time)
         self.general_graph(self.distance_meters,cm,title=None,xlabel=None,ylabel=None)
         # this will store the model data using the ka with the least error
         # (with kd = 0.23)
@@ -115,56 +117,49 @@ class StreeterPhelps(object):
 
         self.graph_two_lines(self.distance_meters, self.DO, self.distance_meters, coptimum)
 
+    @staticmethod
+    def scatterplot3D(x, y, z, title, xlabel, ylabel, zlabel):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, c='g', marker='.')
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+        plt.show()
+
+    @staticmethod
+    def triangle3dplot(x,y,z):
+
+        #x = kas2
+        #y = kds2
+        #z = RMSE2
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_title("root_mean_square_error versus ka and kd")
+        ax.set_xlabel('ka (1/day)')
+        ax.set_ylabel('kd (1/day)')
+        ax.set_zlabel('root_mean_square_error')
+        ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0.1)
+        plt.show()
+
+    @staticmethod
+    def graphtwolines(x1,y1,x2,y2):
+        plt.plot(x1,y1,label = "Optimum Model Curve")
+        plt.plot(x2,y2,'.',label= "Field Data Points")
+        plt.title("Concentration of D.O. (mg/L) versus Distance (meters)")
+        plt.grid(True)
+        plt.xlabel("Distance (meters)")
+        plt.ylabel("DO (mg/L)")
+        plt.legend(loc = "upper right")
+        plt.show()
 
 
-sp = StreeterPhelps()
-sp.find_ka_given_kd()
-sp.fit_models_to_data_ka_and_kd_unknown()
-"""
+if __name__ == '__main__':
+    sp = StreeterPhelps()
+    sp.find_ka_given_kd()
+    sp.fit_models_to_data_ka_and_kd_unknown()
 
 
-def scatterplot3D(x, y, z, title, xlabel, ylabel, zlabel):
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z, c='g', marker='.')
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_zlabel(zlabel)
-    plt.show()
-
-def triangle3dplot():
-    from mpl_toolkits.mplot3d import Axes3D
-    from matplotlib import cm
-    import matplotlib.pyplot as plt
-    import numpy as np
-    x = kas2
-    y = kds2
-    z = RMSE2
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_title("root_mean_square_error versus ka and kd")
-    ax.set_xlabel('ka (1/day)')
-    ax.set_ylabel('kd (1/day)')
-    ax.set_zlabel('root_mean_square_error')
-    ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0.1)
-    plt.show()
-
-#find the the minimum error in the RMSE2 list. find the index (location) of that value. apply that location to kas list to find the
-#optimum ka and kd with the smalest error.
-
-
-def graphtwolines(x1,y1,x2,y2):
-    plt.plot(x1,y1,label = "Optimum Model Curve")
-    plt.plot(x2,y2,'.',label= "Field Data Points")
-    plt.title("Concentration of D.O. (mg/L) versus Distance (meters)")
-    plt.grid(True)
-    plt.xlabel("Distance (meters)")
-    plt.ylabel("DO (mg/L)")
-    plt.legend(loc = "upper right")
-    plt.show()
-data1 = np.array([kas,RMSES]).T
-data2 = np.array([kas2,kds2,RMSE2]).T
-"""
